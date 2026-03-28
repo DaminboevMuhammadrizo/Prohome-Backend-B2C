09224d5b32f2ebb0178db06501ccb2d020aec9f4
Muhammadrizo Daminboev <m701rizo@gmail.com>
2026-03-28 17:48:24 +0500

    code fixing

--- src/modules/user/master/master.module.ts
+++ src/modules/user/master/master.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MasterProfileController } from './master-profile/master-profile.controller';
 import { MasterProfileService } from './master-profile/master-profile.service';
-import { SalaryController } from './salary/salary.controller';
-import { SalaryService } from './salary/salary.service';
 import { MasterJobController } from './master-job/master-job.controller';
 import { MasterJobService } from './master-job/master-job.service';
 import { MasterRatingController } from './master-rating/master-rating.controller';
 import { MasterRatingService } from './master-rating/master-rating.service';
 
 @Module({
-  controllers: [MasterProfileController, SalaryController, MasterJobController, MasterRatingController],
-  providers: [MasterProfileService, SalaryService, MasterJobService, MasterRatingService]
+    controllers: [MasterProfileController, MasterJobController, MasterRatingController],
+    providers: [MasterProfileService, MasterJobService, MasterRatingService]
 })
-export class MasterModule {}
+export class MasterModule { }
