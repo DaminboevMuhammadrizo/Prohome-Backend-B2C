aa205bcca7334e80e63cf4441d28925782ae171b
Muhammadrizo Daminboev <m701rizo@gmail.com>
2026-03-28 17:28:22 +0500

    aded workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { RegionModule } from './modules/region/region.module';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './modules/user/user.module';
-import { JobModule } from './modules/job/job.module';
-import { BannerModule } from './modules/banner/banner.module';
-import { ApartmentModule } from './modules/apartment/apartment.module';
 import { ModulesModule } from './modules/modules.module';
 
 @Module({
