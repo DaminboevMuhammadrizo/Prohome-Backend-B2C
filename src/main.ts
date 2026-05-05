9d22faa060799d739a831450880cdb66512e44c6
Muhammadrizo Daminboev <m701rizo@gmail.com>
2026-05-05 15:29:02 +0500

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,39 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      transformOptions: {
-      enableImplicitConversion: true,
-    },
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true,
+            },
+        }),
+    );
 
-  const bigIntPrototype = BigInt.prototype as BigInt & {
-    toJSON?: () => string;
-  };
-  bigIntPrototype.toJSON = function () {
-    return this.toString();
-  };
+    const bigIntPrototype = BigInt.prototype as BigInt & {
+        toJSON?: () => string;
+    };
+    bigIntPrototype.toJSON = function () {
+        return this.toString();
+    };
 
-  app.enableCors({
-    origin: '*',
-  });
-  const config = new DocumentBuilder()
-    .setTitle('ProHome B2C API')
-    .setDescription('ProHome B2C backend API documentation')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+    app.enableCors({
+        origin: '*',
+    });
+    const config = new DocumentBuilder()
+        .setTitle('ProHome B2C API')
+        .setDescription('ProHome B2C backend API documentation')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-  await app.listen(process.env.PORT ?? 4000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+    await app.listen(process.env.PORT ?? 4000);
 }
 
 bootstrap();
